449a50f3cc67ecd14db67401fefd42a50068e1d9
Bouquineuse <bouquineur@gmx.de>
2020-05-10 21:10:21 +0200

    feat: Improved styling

--- calc-app/src/app/app.module.ts
+++ calc-app/src/app/app.module.ts
@@ -3,9 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorModule } from './calculator/calculator.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CalculatorModule],
+  imports: [
+    BrowserModule,
+    CalculatorModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
