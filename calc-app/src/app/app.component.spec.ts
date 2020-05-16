bf8879b4659e99b2c4450b29f52253110b850e1b
Bouquineuse <bouquineur@gmx.de>
2020-05-16 11:12:11 +0200

    test: add some tests, remove others

--- calc-app/src/app/app.component.spec.ts
+++ calc-app/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +14,49 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calc-app'`, () => {
+  it(`should have as title 'Kopfrechen-Trainer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calc-app');
+    expect(app.title).toEqual('Kopfrechen-Trainer');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('calc-app app is running!');
+  describe('generateTask', () => {
+    it('should generate addition task', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectedOperations = ['+'];
+      app.generateTask();
+      expect(app.calcTask.operand).toEqual('+');
+    });
+
+    it('should generate subtraction task', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectedOperations = ['-'];
+      app.generateTask();
+      expect(app.calcTask.operand).toEqual('-');
+    });
+
+    it('should generate addition task with operands 10 and 20', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectedOperations = ['+'];
+      app.configs[0].rangeFirst = [10, 10];
+      app.configs[0].rangeSecond = [20, 20];
+      app.generateTask();
+      expect(app.calcTask.first).toEqual(10);
+      expect(app.calcTask.second).toEqual(20);
+    });
+
+    it('should generate subtraction task with operands 10 and 20', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.selectedOperations = ['-'];
+      app.configs[1].rangeFirst = [10, 10];
+      app.configs[1].rangeSecond = [20, 20];
+      app.generateTask();
+      expect(app.calcTask.first).toEqual(10);
+      expect(app.calcTask.second).toEqual(20);
+    });
   });
 });
